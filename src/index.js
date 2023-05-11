4a060295ce4c0a20d25f0923a3e3de3fc4b63c42
CassieV <obicassievirtue@gmail.com>
2023-05-11 20:36:09 +0200

    Add button for placing ship

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ const createBoardUI = (boardElement, gameBoardInstance) => {
       const cell = document.createElement('div');
       cell.classList.add('cells');
       
+      boardElement.id === 'playerBoard' ? cell.classList.add('playerCell') : cell.classList.add('computerCell');
       cell.setAttribute('data-row', i);
       cell.setAttribute('data-col', j);
       
@@ -30,31 +31,27 @@ createBoardUI(playerBoard, playerGameBoard);
 createBoardUI(computerBoard, computerGameBoard);
 
 // create ships in different sizes
-const sailBoat = Ship(1);
-const ship = Ship(2);
-const battleShip = Ship(3);
-const aircraftCarrier = Ship(4);
+// const sailBoat = Ship(1);
+// const ship = Ship(2);
+// const battleShip = Ship(3);
+// const aircraftCarrier = Ship(4);
 
-const cells = document.querySelectorAll('.cells');
 
-function clickCell(clickEvent) {
-  for (const cell of cells) {
-    cell.addEventListener('click', clickEvent)
-  }
-}
+const playerCells = document.querySelectorAll('.playerCell');
+const computerCells = document.querySelectorAll('.computerCell');
+const randomBtn = document.getElementById('placeShips');
 
 function placeRandomizedShips(ship) {
   let rand = Math.floor(Math.random() * 2);
   let orientation = (rand === 0) ? 'horizontal' : 'vertical';
-
   let row, col;
 
   if (orientation === 'horizontal') {
-    row = Math.floor(Math.random() * 10);
-    col = Math.floor(Math.random() * (10 - (ship.shipLength - 1)));
-  } else {
     row = Math.floor(Math.random() * (10 - (ship.shipLength - 1)));
     col = Math.floor(Math.random() * 10);
+  } else {
+    row = Math.floor(Math.random() * 10);
+    col = Math.floor(Math.random() * (10 - (ship.shipLength - 1)));
   }
 
   if (playerGameBoard.isSpaceOccupied(ship, row, col, orientation)) {
@@ -63,18 +60,58 @@ function placeRandomizedShips(ship) {
     playerGameBoard.placeShip(ship, row, col, orientation);
   }
 }
-placeRandomizedShips(sailBoat);
-placeRandomizedShips(sailBoat);
-placeRandomizedShips(sailBoat);
-placeRandomizedShips(sailBoat);
-placeRandomizedShips(ship);
-placeRandomizedShips(ship);
-placeRandomizedShips(ship);
-placeRandomizedShips(battleShip);
-placeRandomizedShips(battleShip);
-placeRandomizedShips(aircraftCarrier);
+// create sismilar function for computer board
+function placeComputerShips(ship) {
+  let rand = Math.floor(Math.random() * 2);
+  let orientation = (rand === 0) ? 'horizontal' : 'vertical';
+  let row, col;
 
-function checkCell(event) {
+  if (orientation === 'horizontal') {
+    row = Math.floor(Math.random() * (10 - (ship.shipLength - 1)));
+    col = Math.floor(Math.random() * 10);
+  } else {
+    row = Math.floor(Math.random() * 10);
+    col = Math.floor(Math.random() * (10 - (ship.shipLength - 1)));
+  }
+
+  if (computerGameBoard.isSpaceOccupied(ship, row, col, orientation)) {
+    placeComputerShips(ship);
+  } else {
+    computerGameBoard.placeShip(ship, row, col, orientation);
+  }
+}
+
+// create function placing ships in random areas on player's gameboard
+function randomShipBtn() {
+  for (let i = 0; i < 4; i++) {
+    placeRandomizedShips(Ship(1));
+  }
+  for (let i = 0; i < 3; i++) {
+    placeRandomizedShips(Ship(2));
+  }
+  for (let i = 0; i < 2; i++) {
+    placeRandomizedShips(Ship(3)); 
+  }
+  placeRandomizedShips(Ship(4)); 
+}
+randomBtn.addEventListener('click', randomShipBtn);
+
+// create function placing ships in random areas on computer's gameboard
+function placeComputerShipsOnLoad() {
+  for (let i = 0; i < 4; i++) {
+    placeComputerShips(Ship(1)); 
+  }
+  for (let i = 0; i < 3; i++) {
+    placeComputerShips(Ship(2)); 
+  }
+  for (let i = 0; i < 2; i++) {
+    placeComputerShips(Ship(3)); 
+  }
+  placeComputerShips(Ship(4)); 
+}
+placeComputerShipsOnLoad();
+
+function checkPlayerCell(event) {
   const cell = event.target;
   const row = cell.getAttribute('data-row');
   const col = cell.getAttribute('data-col');
@@ -86,5 +123,24 @@ function checkCell(event) {
     cell.style.backgroundColor = 'indianred';
   }
 }
+function checkComputerCell(event) {
+  const cell = event.target;
+  const row = cell.getAttribute('data-row');
+  const col = cell.getAttribute('data-col');
+  const cellsArray = computerGameBoard.getBoard();
 
-clickCell(checkCell);
+  if (!cellsArray[row][col].ship) {
+    cell.style.backgroundColor = 'lightgrey';
+  } else {
+    cell.style.backgroundColor = 'indianred';
+  }
+}
+
+
+for (const cell of playerCells) {
+  cell.addEventListener('click', checkPlayerCell);
+}
+
+for (const cell of computerCells) {
+  cell.addEventListener('click', checkComputerCell);
+}
